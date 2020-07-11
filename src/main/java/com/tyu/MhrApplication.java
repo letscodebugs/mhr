92621e1b4de2bfb2ecfa55862fe8e57af8fad0f7
tyu hwuang <tyu.hwuang@gmail.com>
2020-07-11 09:11:46 -0500

    create mhr project of springboot

---
+++ src/main/java/com/tyu/MhrApplication.java
@@ -0,0 +1,13 @@
+package com.tyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MhrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tyu/MhrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
